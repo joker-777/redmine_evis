dacc1652a98e3f5b4aabdd42c14c3088953a8101
Johannes Köhler <joker777vs.666@gmail.com>
2010-10-02 19:38:30 +0200

    initialized the plugin

---
+++ lib/redmine_evis.rb
@@ -0,0 +1,12 @@
+# RedmineEvis
+#
+
+
+
+class News < ActiveRecord::Base
+  def test_bla
+    puts "sdf"
+
+  end
+
+end
---
+++ spec/redmine_evis_spec.rb
@@ -0,0 +1,9 @@
+require File.dirname(__FILE__) + '/spec_helper'
+
+
+describe "News as Evis" do
+  it "should request Evis" do
+    bla = 1
+    bla.should 4
+  end
+end
